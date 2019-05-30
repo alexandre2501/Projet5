6dfa4aff7e3df0569573d5bc9d5d083dac01351c
Unknown <urbanski.alexandre@gmail.com>
2019-05-30 11:56:45 +0200

    ajustement style mes repas

--- resources/views/appTemplate.blade.php
+++ resources/views/appTemplate.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="node_modules/materialize-css/dist/css/materialize.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <title>Projet 5</title>
 </head>
 <body class="">
--- resources/views/includesApp/appMesRepas.blade.php
+++ resources/views/includesApp/appMesRepas.blade.php
@@ -1,14 +1,14 @@
 <div v-else-if="appContent === 'mesRepas'">
     <h2>Mes Repas</h2>
-    <div class="row">
-        <div id="dateBlock" class="col s12">
-            <div id="previousDate" class="col s2" v-on:click="loadPreviousDate">Previous</div>
-            <div id="nameDate" class="col s8">@{{ mealsData[dateIndex].lisibleDate }}</div>
-            <div id="nextDate" class="col s2" v-on:click="loadNextDate">Next</div>
+    <div class="row repas-block">
+        <div id="date-block" class="col s12 valign-wrapper">
+            <div id="previousDate" class="col s2 center-align" v-on:click="loadPreviousDate"><i class="fas fa-3x fa-chevron-circle-left"></i></div>
+            <div id="nameDate" class="col s8 center-align">@{{ mealsData[dateIndex].lisibleDate }}</div>
+            <div id="nextDate" class="col s2 center-align" v-on:click="loadNextDate"><i class="fas fa-3x fa-chevron-circle-right"></i></div>
         </div>
-        <div id="mealBlock" class="col s12">
-            <div id="mealsContent" class="col s9">Meals</div>
-            <div id="foodsContent" class="col s3">Foods</div>
+        <div id="meal-block" class="col s12">
+            <div id="meals-content" class="col s9">Meals</div>
+            <div id="foods-content" class="col s3">Foods</div>
         </div>
     </div>
 </div>
