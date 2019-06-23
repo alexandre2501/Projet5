37135eff6ffaf51c23ddb3db13a608d6f7a767a4
Unknown <urbanski.alexandre@gmail.com>
2019-06-23 15:45:10 +0200

    ajout d'icones dans la sidebar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,10 +12,10 @@
                         <p>{{Auth::user()->name}}</p>
                     </div>
                 </div>
-                <li v-on:click="loadAccueilHtml"><a>Accueil</a></li>
-                <li v-on:click="loadProfilHtml"><a>Mon Profil</a></li>
-                <li v-on:click="loadMaNourritureHtml"><a>Ma Nourriture</a></li>
-                <li v-on:click="loadMesRepasHtml"><a>Mes Repas</a></li>
+                <li v-on:click="loadAccueilHtml"><a><i class="fas fa-home"></i>Accueil</a></li>
+                <li v-on:click="loadProfilHtml"><a><i class="fas fa-user"></i>Mon Profil</a></li>
+                <li v-on:click="loadMaNourritureHtml"><a><i class="fas fa-apple-alt"></i>Ma Nourriture</a></li>
+                <li v-on:click="loadMesRepasHtml"><a><i class="fas fa-utensils"></i>Mes Repas</a></li>
             </ul>
         </div>
         <div id="app_content" class="col s10 offset-s2 #eeeeee grey lighten-3">
