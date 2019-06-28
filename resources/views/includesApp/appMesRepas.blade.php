a9b249f6b294adbd44bf4f550c1e509994ab4695
Unknown <urbanski.alexandre@gmail.com>
2019-06-28 15:52:00 +0200

    encore plus de style

--- resources/views/includesApp/appMesRepas.blade.php
+++ resources/views/includesApp/appMesRepas.blade.php
@@ -1,6 +1,6 @@
 <div v-else-if="appContent === 'mesRepas'">
     <h2>Mes Repas</h2>
-    <div class="row repas-block">
+    <div class="row repas-block #fafafa grey lighten-5 z-depth-2">
         <div id="date-block" class="col s12 valign-wrapper">
             <div id="previousDate" class="col s2 center-align" v-on:click="loadPreviousDate"><i class="fas fa-3x fa-chevron-circle-left"></i></div>
             <div id="nameDate" class="col s8 center-align">@{{ mealsData[dateIndex].lisibleDate }}</div>
@@ -9,39 +9,38 @@
         <div id="meal-block" class="col s12">
             <div id="meals-content" class="col s9">
                 <div class="col s12">
-                    <h3 class="col s12">CONSO DU JOUR</h3>
-                    <div class="col s3">Calories : @{{ totalCal }} cal</div>
-                    <div class="col s3">Proteines : @{{ totalPro }} g</div>
-                    <div class="col s3">Lipides : @{{ totalLip }} g</div>
-                    <div class="col s3">Glucides : @{{ totalGlu }} g</div>
+                    <h3 class="col s12 center-align">CONSO DU JOUR</h3>
+                    <div class="col s3 center-align">Calories : @{{ totalCal }} cal</div>
+                    <div class="col s3 center-align">Proteines : @{{ totalPro }} g</div>
+                    <div class="col s3 center-align">Lipides : @{{ totalLip }} g</div>
+                    <div class="col s3 center-align">Glucides : @{{ totalGlu }} g</div>
                 </div>
-                <div class="col s3" v-for="(value, index) in mealsData[dateIndex].meals">
-                    <div class="col s12">@{{ value.name }}<i class="far fa-times-circle valign-wrapper" v-on:click="deleteFoodFromMeal(index)"></i></div>
-                    <div class="col s6 valign-wrapper"><input type="number" class="col s8" v-model="value.cal" @keyup="checkNumeric(index, 'cal')"><span>cal</span></div>
-                    <div class="col s6 valign-wrapper"><input type="number" class="col s8" v-model="value.quant" @keyup="checkNumeric(index, 'quant')"><span>g</span></div>
-                    <div class="col s4 valign-wrapper"><input type="number" class="col s8" v-model="value.pro" @keyup="checkNumeric(index, 'pro')"><span>g</span></div>
-                    <div class="col s4 valign-wrapper"><input type="number" class="col s8" v-model="value.lip" @keyup="checkNumeric(index, 'lip')"><span>g</span></div>
-                    <div class="col s4 valign-wrapper"><input type="number" class="col s8" v-model="value.glu" @keyup="checkNumeric(index), 'glu'"><span>g</span></div>
+                <div class="col s3 meal-content" v-for="(value, index) in mealsData[dateIndex].meals">
+                    <h6 class="col s12 center-align">@{{ value.name }}<i class="far fa-times-circle valign-wrapper" v-on:click="deleteFoodFromMeal(index)"></i></h6>
+                    <div class="col s6 pad-zero"><div class="col s12 center-align #d1c4e9 deep-purple lighten-4">Calorie</div><div class="col s8 valign-wrapper"><input type="number" v-model="value.cal" @keyup="checkNumeric(index, 'cal')"><span>cal</span></div></div>
+                    <div class="col s6 pad-zero"><div class="col s12 center-align #dce775 lime lighten-2">Quantité</div><div class="col s8 valign-wrapper"><input type="number" v-model="value.quant" @keyup="checkNumeric(index, 'quant')"><span>g</span></div></div>
+                    <div class="col s4 pad-zero"><div class="col s12 center-align #aed581 light-green lighten-2">Protéines</div><div class="col s12 valign-wrapper"><input type="number" v-model="value.pro" @keyup="checkNumeric(index, 'pro')"><span>g</span></div></div>
+                    <div class="col s4 pad-zero"><div class="col s12 center-align #a1887f brown lighten-2">Lipides</div><div class="col s12 valign-wrapper"><input type="number" v-model="value.lip" @keyup="checkNumeric(index, 'lip')"><span>g</span></div></div>
+                    <div class="col s4 pad-zero"><div class="col s12 center-align #f06292 pink lighten-2">Glucides</div><div class="col s12 valign-wrapper"><input type="number" v-model="value.glu" @keyup="checkNumeric(index), 'glu'"><span>g</span></div></div>
                 </div>
                 <div class="col s12 center-align">
                     <button v-on:click="updateMeals">Confirmer</button>
                 </div>
             </div>
             <div id="foods-content" class="col s3">
-                <h6>Food</h6>
+                <h3 class="col s12 center-align">Food</h3>
                 <div class="col s12 valign-wrapper" v-for="(value,index) in userFood">
                     <div class="col s2"><i v-on:click="addFoodToMeal(index)" class="fas fa-2x fa-plus-circle"></i></div>
-                    <div class="col s10">
-                        <p></p>
+                    <div class="col s10 food-desc">
                         <div class="col s12 center-align">@{{ value.name }}</div>
                         <div class="col s6">Calories : @{{ value.cal }} cal</div>
                         <div class="col s6">Quantité : @{{ value.quant }} g</div>
                     </div>
                 </div>
-                <h6>Dish</h6>
+                <h3 class="col s12 center-align">Dish</h3>
                 <div class="col s12 valign-wrapper" v-for="(value,index) in userDish">
                     <div class="col s2"><i v-on:click="addDishToMeal(index)" class="fas fa-2x fa-plus-circle"></i></div>
-                    <div class="col s10">
+                    <div class="col s10 food-desc">
                         <div class="col s12 center-align">@{{ value.name }}</div>
                         <div class="col s6">Calories : @{{ value.cal }} cal</div>
                         <div class="col s6">Quantité : @{{ value.quant }} g</div>
