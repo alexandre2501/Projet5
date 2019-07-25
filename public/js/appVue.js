31d8e1f902fc6df7c496130fab52d3beb4c73345
Unknown <urbanski.alexandre@gmail.com>
2019-07-25 17:25:09 +0200

    transformation de la partie profil en composant

--- public/js/appVue.js
+++ public/js/appVue.js
@@ -1,4 +1,194 @@
-//const axios = require('axios');
+const store = new Vuex.Store({
+    state: {
+        env: '',
+        currentRoute: '/acceuil',
+        //User Data
+        userData: '',
+        userAvatarLink: '',
+        userDish: [],
+        userFood: [],
+        content: '',
+        contentHtml: '<p>coucou</p>',
+        appContent: 'accueil',
+        csrfToken: document.head.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+        //Changement de mot de passe
+        passRegex: '^((?=\\S*?[a-z]).{7,})\\S$',
+        passChangeBtn: 1,
+        passMsg: '',
+        test: null,
+        //Page Ma Nourriture
+        myFood: 'myMeal',
+        //Format du formulaire nourriture (update or create)
+        foodFormState: '',
+        //Erreur formulaire ajout de nourriture
+        foodFormError: Object.assign({}, modelFoodError),
+        //Objet nourriture à update
+        foodToUpdate: '',
+        //Format du formulaire plat (update or create)
+        dishFormState: '',
+        //Erreur formulaire ajout de plat
+        dishFormError: Object.assign({}, modelDishError),
+        //Objet plat à update
+        dishToUpdate: '',
+        //Page Mes Repas
+        //On stock la date du jour pour construire le calendrier des repas
+        frenchDayName: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        frenchMonthName: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        actualDate: new Date(),
+        dateIndex: 6,
+        mealsData: [],
+        totalCal: 0,
+        totalPro: 0,
+        totalLip: 0,
+        totalGlu: 0,
+    },
+    mutations: {
+        setEnv(state, env){
+            state.env = env;
+        },
+        setUserData(state, data){
+            state.userData = data;
+        },
+        setUserAvatarLink(state, link){
+            state.userAvatarLink = link;
+        },
+        getUserMeal(){
+
+        },
+    }
+})
+
+const Profil = Vue.component('Profil', {
+    template: '<div class="row flex" id="profil_block">\n' +
+    '    <div id="change_avatar_div" class="col s12 m12 l6">\n' +
+    '        <div class="#fafafa grey lighten-5 content_blocks z-depth-2">\n' +
+    '        <h3 class="center-align">Photo de profil</h3>\n' +
+    '            <div class="pad-block">\n' +
+    '        <img id="avatar_min" alt="Miniature du profil" :src="$store.state.userAvatarLink">\n' +
+    '        <form id="upload_new_avatar">\n' +
+    '            <div class="file-field input-field">\n' +
+    '                <div class="btn">\n' +
+    '                    <span>File</span>\n' +
+    '                    <input id="upload_avatar_input" type="file" name="avatar" accept="image/*">\n' +
+    '                </div>\n' +
+    '                <div class="file-path-wrapper">\n' +
+    '                    <input class="file-path validate" type="text">\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '            <button class="btn" v-on:click="uploadNewAvatar" type="submit" id="submit_new_avatar">Modifier</button>\n' +
+    '        </form>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '    <div id="change_password_div" class="col s12 m12 l6">\n' +
+    '        <div class="#fafafa grey lighten-5 content_blocks z-depth-2">\n' +
+    '        <h3 class="center-align">Changer de mot de passe</h3>\n' +
+    '        <div class="pad-block">\n' +
+    '        <form id="change_password_form">\n' +
+    '            <div class="input-field col s12">\n' +
+    '                <input v-model="oldPass" id="old_pass_input" type="password" name="oldPass">\n' +
+    '                <label for="old_pass_input">Ancien mot de passe :</label>\n' +
+    '            </div>\n' +
+    '            <div class="input-field col s12">\n' +
+    '                <input v-model="newPass" v-on:keyup="comparePassword" id="new_pass_input" type="password" name="newPass" >\n' +
+    '                <label for="new_pass_input">Nouveau mot de passe :</label>\n' +
+    '            </div>\n' +
+    '            <div class="input-field col s12">\n' +
+    '                <input v-model="confirmPass" v-on:keyup="comparePassword" id="confirm_pass_input" type="password" name="confirmPass">\n' +
+    '                <label for="confirm_pass_input">Confirme le mot de passe :</label>\n' +
+    '            </div>\n' +
+    '            <button type="submit" id="submit_password_change" v-on:click="submitPasswordChange" :disabled="passChangeBtn === 1 ? true : false">Modifier</button>\n' +
+    '        </form>\n' +
+    '        <span>{{passMsg}}</span>\n' +
+    '        <div><p><strong>Tips :</strong> Votre mot de passe ne doit pas contenir d\'espace et doit faire 8 caractères minimum</p></div>\n' +
+    '        </div>\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '</div>',
+    data: function(){
+        return{
+            userAvatarLink: '',
+            passChangeBtn: '',
+            passMsg: '',
+            oldPass: null,
+            newPass: null,
+            confirmPass: null,
+        }
+    },
+    methods: {
+        uploadNewAvatar(e){
+            e.preventDefault();
+            var formData = new FormData();
+            var file = document.getElementById('upload_avatar_input').files[0];
+            formData.append('avatar', file);
+            axios.post('/upload/avatar',
+                formData
+                ,{
+                    headers: {
+                        //'content-type': 'multipart/form-data',
+                    }
+                })
+                .then(function(response){
+                    this.$parent.setUserData();
+                    console.log(response.data);
+                }.bind(this))
+                .catch(function(error){
+                    console.log(error);
+                })
+        },
+        checkPassword(){
+            if(this.newPass === null || this.newPass === '' ||
+                this.oldPass === null || this.oldPass === '' ||
+                this.confirmPass === null || this.confirmPass === ''){
+                return false;
+            }
+            if(!this.newPass.match(this.passRegex) || !this.confirmPass.match(this.passRegex)){
+                return false;
+            }
+            else{
+                return true;
+            }
+        },
+        comparePassword(){
+            if(!this.checkPassword()){
+                this.passChangeBtn = 1;
+                this.passMsg = 'Le mot de passe n\'est pas conforme';
+            }
+            else{
+                if(this.newPass != this.confirmPass){
+                    this.passChangeBtn = 1;
+                    this.passMsg = 'Les mots de passe ne sont pas identique';
+                }
+                else{
+                    this.passChangeBtn = 0;
+                    this.passMsg = '';
+                }
+            }
+        },
+        submitPasswordChange(e){
+            e.preventDefault();
+            axios.post('/password/change',{
+                oldPass: this.oldPass,
+                newPass: this.newPass,
+            },{
+                headers: {
+                    'Content-Type' : 'application/json; charset=UTF-8',
+                }
+            })
+                .then(function(response){
+                    if(response.data.success != null){
+                        this.passMsg = response.data.success;
+                    }
+                    else {
+                        this.passMsg = response.data.error;
+                    }
+                }.bind(this))
+                .catch(function(error){
+                    console.log(error);
+                })
+        },
+    },
+})
 
 var modelFoodError = {
     foodNameError: '',
@@ -20,7 +210,7 @@ var modelDishError = {
 
 const routes = [
     { path: '/accueil'},
-    { path: '/profil'},
+    { path: '/profil', component: Profil},
     { path: '/food'},
     { path: '/meal'},
 ]
@@ -30,18 +220,20 @@ const router = new VueRouter({
 })
 
 var app = new Vue({
-    router,
     el: '#app',
+    components: {'Profil': Profil},
+    router,
+    store,
     mounted(){
         window.axios.defaults.headers.common['X-CSRF-TOKEN'] = this.csrfToken;
         M.AutoInit();
         this.setEnv();
         this.setUserData();
         this.getUserMeal();
-        this.checkRoute();
+        //this.checkRoute();
     },
     watch: {
-        '$route' (to, from){
+        /*'$route' (to, from){
             if(to.path === '/acceuil'){
                 this.loadAccueilHtml();
             }
@@ -57,7 +249,7 @@ var app = new Vue({
             else{
                 this.loadAccueilHtml();
             }
-        }
+        }*/
     },
     data: {
         //avatar: null,
@@ -74,9 +266,6 @@ var app = new Vue({
         csrfToken: document.head.querySelector('meta[name="csrf-token"]').getAttribute('content'),
         //Changement de mot de passe
         passRegex: '^((?=\\S*?[a-z]).{7,})\\S$',
-        oldPass: document.getElementById('old_pass_input').value,
-        newPass: document.getElementById('new_pass_input').value,
-        confirmPass: document.getElementById('confirm_pass_input').value,
         passChangeBtn: 1,
         passMsg: '',
         test: null,
@@ -135,15 +324,12 @@ var app = new Vue({
               }
           }
         },
-        avatar(e){
-            console.log(this,e);
-        },
         setEnv(){
           var env = window.location.href;
           env = env.substr(7);
           var id = env.search('/');
           env = env.substr(0,id);
-          this.env = env;
+          this.$store.commit('setEnv', env);//.env = env;
         },
         setUserData(){
             axios.get('/auth/data',{
@@ -152,8 +338,9 @@ var app = new Vue({
                 }
             })
                 .then(function(response){
-                    this.userData = response.data;
-                    this.userAvatarLink = 'http://' + this.env + '/avatars/' + this.userData.avatar;
+                    this.$store.commit('setUserData', response.data)//.userData = response.data;
+                    var link = 'http://' + this.$store.state.env + '/avatars/' + this.$store.state.userData.avatar;
+                    this.$store.commit('setUserAvatarLink', link)//.userAvatarLink = 'http://' + this.env + '/avatars/' + this.userData.avatar;
                 }.bind(this))
                 .catch(function(error){
                     console.log(error);
