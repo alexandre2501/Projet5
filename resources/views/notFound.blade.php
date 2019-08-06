0df8977a440fda6b2466a735beedb3eacb9b169e
Unknown <urbanski.alexandre@gmail.com>
2019-08-06 13:23:46 +0200

    creation view notFound

---
+++ resources/views/notFound.blade.php
@@ -0,0 +1,10 @@
+@extends('template')
+
+@section('contenu')
+    <div class="container">
+        <div class="row">
+            <p>Oops, cette page n'existe pas</p>
+            <p><a href="/home">Retourner à l'accueil</a></p>
+        </div>
+    </div>
+    @endsection
