d39905a850d9e90fdc85247aa2dcf9f158676c07
Unknown <urbanski.alexandre@gmail.com>
2019-08-06 17:02:18 +0200

    view admin template + admin home

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+@extends('admin/adminTemplate')
+
+@section('contenu')
+
+    <div id="app_content" class="col s12 l10 offset-l2 #eeeeee grey lighten-3">
+        <div class="app-box z-depth-2 #e3f2fd blue lighten-5">
+            Accueil ADMIN
+        </div>
+    </div>
+
+@endsection
