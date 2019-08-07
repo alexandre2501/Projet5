beaaa9e61eab833c9e897640289b85f009b04785
Unknown <urbanski.alexandre@gmail.com>
2019-08-07 15:33:47 +0200

    decoupage du chargement des scripts/stylesheet

---
+++ resources/views/admin/script.blade.php
@@ -0,0 +1,9 @@
+<script src="/node_modules/materialize-css/dist/js/materialize.js"></script>
+<!--<script src="/node_modules/vue/dist/vue.js"></script>
+<script src="/node_modules/vuex/dist/vuex.js"></script>
+<script src="/node_modules/vue-router/dist/vue-router.js"></script>
+<script src="/node_modules/axios/dist/axios.js"></script>-->
+<script>
+    M.AutoInit();
+</script>
+<link rel="stylesheet" href="/css/adminStyle.css">
